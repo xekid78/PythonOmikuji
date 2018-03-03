b63270cbb442b1ec87c894c1e15aefd3f98fb37b
Makoto Tanabe <xekid78@outlook.com>
2018-03-03 16:02:43 +0900

    Add files via upload

---
+++ omikuji.py
@@ -0,0 +1,14 @@
+import random
+omikuji = random.randint(1,6)
+if omikuji == 1:
+    print("大吉")
+elif omikuji == 2:
+    print("中吉")
+elif omikuji == 3:
+    print("小吉")
+elif omikuji == 4:
+    print("吉")
+elif omikuji == 5:
+    print("凶")
+else:
+    print("大凶")
